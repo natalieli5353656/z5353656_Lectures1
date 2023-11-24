cae21c77d54d10c8e65c4174fd0e2e3734c24083
z5353656_lecture <lingyunli99@gmail.com>
2023-11-24 21:33:23 +1100

    update

--- lecture/lec_pd_dataframes.py
+++ lecture/lec_pd_dataframes.py
@@ -63,7 +63,7 @@ bday_ser = pd.Series(data=bday, index=dates)
 #   Create a dataframe
 # ---------------------------------------------------------------------------- 
 # Data Frame with close and Bday columns
-df  = '?'
+df  = pd.DataFrame({'Close': prc_ser, 'bDay': bday_ser})
 #print(df) 
 
 
@@ -123,7 +123,7 @@ new_ser = pd.Series(data=[1,3,2], index=['a', 'c', 'b'])
 #print(new_ser.is_monotonic_increasing) 
 
 # Sort the series based on the index
-sorted_ser  = '?'
+sorted_ser  = new_ser.sort_index()
 #print(sorted_ser) 
 
 # This will return only the first rows (not the entire series as before)
@@ -144,7 +144,7 @@ sorted_ser  = '?'
 # dtype: int64
 
 # Create a series with an unsorted index 
-ser_sort_inplace  = '?'
+ser_sort_inplace  = pd.Series(data=[1,2,3], index=['a','b','c'])
 
 # Sort the series. Note that we are not assigning this function call
 # to a new variable.
--- lecture/lec_pd_series.py
+++ lecture/lec_pd_series.py
@@ -39,24 +39,24 @@ prices = [
 #   Create a Series instance
 # ---------------------------------------------------------------------------- 
 # Create a series object
-ser  = '?'
+ser  = pd.Series(data=prics, index=index)
 #print(ser) 
 
 # Select Qantas price on '2020-01-02' ($7.16) using ...
 
 # ... the `prices` list
-prc0  = '?'
+prc0  =  prices[dates.index('2020-01-02')]
 #print(prc0) 
 
 # ... the `ser` series
-prc1  = '?'
+prc1  = ser['2020-01-02']
 #print(prc1)                     
 
 # ---------------------------------------------------------------------------- 
 #   Slicing series
 # ---------------------------------------------------------------------------- 
 # Unlike dictionaries, you can slice a series
-prcs  = '?'
+prcs  = ser['2020-01-06':'2020-01-10']
 #print(prcs) 
 
 # ---------------------------------------------------------------------------- 
@@ -64,7 +64,7 @@ prcs  = '?'
 # ---------------------------------------------------------------------------- 
 
 # Use `.array` to get the underlying data array
-ary  = '?'
+ary  = ser.arry
 #print(ary)  
 
 # Like any instance, you can get its type (i.e., the class used to create the
@@ -72,7 +72,7 @@ ary  = '?'
 #print(type(ser.array)) 
 
 # Use the `index` attribute to get the index from a series
-the_index  = '?'
+the_index  = ser.index
 #print(the_index) 
 
 # Like any instance, you can get its type (i.e., the class used to create the
